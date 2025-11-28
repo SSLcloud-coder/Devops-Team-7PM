62393eca8d4a343ef8e47a4135128a077bcfa856
ssl-cloud <sslcloudlearning@gmail.com>
2025-11-28 20:07:40 +0530

    JIRA 101-NEW-VNET-ADDED-EAST-US

--- main.tf
+++ main.tf
@@ -9,6 +9,14 @@ resource "azurerm_virtual_network" "vnet" {
   location           = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
+
+#JIRA 101-NEW-VNET-ADDED-EAST-US
+resource "azurerm_virtual_network" "vnet-east-us" {
+  name                = "${var.prefix}-vnet-east-us"
+  address_space       = ["10.16.37.88/32"]
+  location           = azurerm_resource_group.rg.location-east-us
+  resource_group_name = azurerm_resource_group.rg.name-east-us
+}
 #changes done by admin
 resource "azurerm_subnet" "subnet" {
   name                 = "${var.prefix}-subnet"
