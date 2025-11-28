389dd39674cd9a4a9a00b377ed6163f296606e22
ssl-cloud <sslcloudlearning@gmail.com>
2025-11-28 20:15:20 +0530

    JIRA 102-NEW-VNET-ADDED-WEST-US

--- main.tf
+++ main.tf
@@ -9,6 +9,13 @@ resource "azurerm_virtual_network" "vnet" {
   location           = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
+
+resource "azurerm_virtual_network" "vnet-west-us" {
+  name                = "${var.prefix}-vnet-west-us"
+  address_space       = ["10.40.84.99/32"]
+  location           = azurerm_resource_group.rg.location-west-us
+  resource_group_name = azurerm_resource_group.rg.name-west-us
+}
 #changes done by admin
 resource "azurerm_subnet" "subnet" {
   name                 = "${var.prefix}-subnet"
