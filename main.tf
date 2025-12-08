9143645dd795ccbd78a302f0e96407999c03a259
ssl-cloud <sslcloudlearning@gmail.com>
2025-12-08 19:36:17 +0530

    JIRA-201-Pankaj

--- main.tf
+++ main.tf
@@ -31,6 +31,14 @@ resource "azurerm_virtual_network" "vnet-east-us" {
   location           = azurerm_resource_group.rg.location-east-us
   resource_group_name = azurerm_resource_group.rg.name-east-us
 }
+
+#JIRA 201-NEW-VNET-ADDED-South-US
+resource "azurerm_virtual_network" "vnet--South--us3" {
+  name                = "${var.prefix}--vnet--South--us3"
+  address_space       = ["10.16.37.88/32"]
+  location           = azurerm_resource_group.rg.location--vnet--South--us3
+  resource_group_name = azurerm_resource_group.rg.name-vnet--South--us3
+}
 #changes done by admin
 resource "azurerm_subnet" "subnet" {
   name                 = "${var.prefix}-subnet"
